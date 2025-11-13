40c79f4a557fa035ccfd9f69500cafd0140165a2
Ian Khoo <ian.t.khoo@gmail.com>
2025-11-12 20:26:28 -0500

    Rename entity from vga_fsmCIAO to vga_fsm

--- vga_fsmCIAO.vhd
+++ vga_fsmCIAO.vhd
@@ -4,59 +4,40 @@ use ieee.std_logic_1164.all;
 library vga;
 use vga.vga_data.all;
 
-entity vga_fsmCIAO is
-	generic (
-		vga_res:	vga_timing := vga_res_default
-	);
-	port (
-		vga_clock:		in	std_logic;
-		reset:			in	std_logic;
+entity vga_fsm is
+    generic (
+        vga_res : vga_timing := vga_res_default
+    );
+    port (
+        vga_clock   : in  std_logic;
+        reset       : in  std_logic;
 
-		point:			out	coordinate;
-		point_valid:	out	boolean;
+        point       : out coordinate;
+        point_valid : out boolean;
 
-		h_sync:			out	std_logic;
-		v_sync:			out std_logic
-	);
-end entity vga_fsmCIAO;
+        h_sync      : out std_logic;
+        v_sync      : out std_logic
+    );
+end entity vga_fsm;
 
-architecture fsm of vga_fsmCIAO is
-	-- internal signal for current position
-	signal current_point: coordinate := make_coordinate(0,0);
-begin
-	-- implement methodology to drive outputs here
-	-- use vga_data functions and types to make your life easier
-	-- main process will advance through all pixel positions
-	process(vga_clock, reset)
-    begin
-        if reset = '0' then  -- active-low reset
-            current_point <= make_coordinate(0, 0);
-        elsif rising_edge(vga_clock) then
-            -- move to next coordinate every clock cycle
-            current_point <= next_coordinate(current_point, vga_res);
-        end if;
-    end process;
+architecture fsm of vga_fsm is
+    -- internal signal for current position
+    signal current_point : coordinate := make_coordinate(0, 0);
 
-    -- output current point
-    point <= current_point;
-    
-    -- check if current point is in visible area
-    point_valid <= point_visible(current_point, vga_res);
-    
     -- raw sync signals
-    signal h_sync_raw : std_logic := '1';
-    signal v_sync_raw : std_logic := '1';
-    signal h_sync_shift : std_logic_vector(15 downto 0) := (others => '0');
-    signal v_sync_shift : std_logic_vector(15 downto 0) := (others => '0');
-
+    signal h_sync_raw    : std_logic := '1';
+    signal v_sync_raw    : std_logic := '1';
+    signal h_sync_shift  : std_logic_vector(15 downto 0) := (others => '0');
+    signal v_sync_shift  : std_logic_vector(15 downto 0) := (others => '0');
 begin
+
     -- main process will advance through all pixel positions
     process(vga_clock, reset)
     begin
         if reset = '0' then  -- active-low reset
             current_point <= make_coordinate(0, 0);
-            h_sync_shift <= (others => '0');
-            v_sync_shift <= (others => '0');
+            h_sync_shift  <= (others => '0');
+            v_sync_shift  <= (others => '0');
         elsif rising_edge(vga_clock) then
             -- move to next coordinate every clock cycle
             current_point <= next_coordinate(current_point, vga_res);
@@ -75,5 +56,10 @@ begin
     h_sync <= h_sync_shift(15);
     v_sync <= v_sync_shift(15);
 
+    -- output current point
+    point <= current_point;
+    
+    -- check if current point is in visible area
+    point_valid <= point_visible(current_point, vga_res);
 
 end architecture fsm;
